e3bddf0175c1eef558ae88384bcfd99e6c4232d4
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-01 14:25:27 +0100

    Working on the error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,23 @@
-
+import { useRouteError, Link} from 'react-router-dom'
 const Error = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Error
-  </h1>
-  )
+  const error = useRouteError();
+  console.log(error);
+
+  if(error.status === 404){
+    return(
+      <main className='grid min-h-[100vh] place-items-center px-8'>
+        <div className='text-center'>
+          <p className='text-9xl font-semibold text-primary'>404</p>
+           <h1 className='mt-4 text-3xl font-bold tracking-tight'>page not found</h1>
+        </div>
+   </main>
+    );
+  }
+
+  return <main className='grid min-h-[100vh] place-items-center px-8'>
+     <h4 className='text-center font-bold text-4xl'> There was an error...</h4>
+  </main>
+  
 }
 
 export default Error
