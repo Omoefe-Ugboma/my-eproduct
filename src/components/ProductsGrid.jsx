dd74c9df996b6bc693a80bb6387c09fd3bb77aa0
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-12 20:08:55 +0100

    Product grid still in progress

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -12,7 +12,11 @@ const ProductsGrid = () => {
           >
            <figure className='px-4 pt-4'>
              <img src={image} alt={title} className='rounded-xl h-64 md:h-48 w-full object-cover' />
-           </figure>   
+           </figure>  
+           <div className='card-body items-center text-center'>
+              <h2 className='card-title capitalize tracking-wider'>{title}</h2>
+              <span className='text-secondary'>{price}</span>
+           </div> 
           </Link>
          })}
     </div>
