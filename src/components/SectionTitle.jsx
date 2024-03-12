0e69a5e8b5d1cc46d71c477a6771bffb09db6bd7
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-12 19:39:31 +0100

    Working on the section Title

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,10 +1,9 @@
 
-const SectionTitle = () => {
-  return (
-    <div>
-      <h4>Title</h4>
-    </div>
-  )
+const SectionTitle = ({text}) => {
+  return <div className="border-b border-base-300 pb-5">
+       <h2>
+        </h2> 
+  </div>
 }
 
 export default SectionTitle
