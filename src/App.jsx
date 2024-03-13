8ff0663eac371156ecb7fb08ca88e0a449e4b163
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-13 14:17:49 +0100

    Single product update

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ import {
 import { ErrorElement } from './components';
 
 // loaders
-  import {loader as landingLoader} from './pages/Landing'
+  import {loader as landingLoader} from './pages/Landing';
+  import {loader as singleProductLoader} from './pages/SingleProduct';
 // actions
 
 const router = createBrowserRouter([
@@ -38,6 +39,8 @@ const router = createBrowserRouter([
       {
         path:'products/:id',
         element:<SingleProduct/>,
+        errorElement: <ErrorElement/>,
+        loader:singleProductLoader,
       },
       {
         path:'cart',
