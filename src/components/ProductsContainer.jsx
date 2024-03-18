20a12ab348fd5ce34beb01850876fc7021e75a02
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-18 16:38:47 +0100

    Working on the product list button for grid and list

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,5 +1,8 @@
-import ProductsGrid from "./ProductsGrid";
-import ProductsList from "./ProductsList";
+import { useLoaderData } from 'react-router-dom';
+import ProductsGrid from './ProductsGrid';
+import ProductsList from './ProductsList';
+import { useState } from 'react';
+import { BsFillGridFill, BsList } from 'react-icons/bs';
 
 const ProductsContainer = () => {
   return (
