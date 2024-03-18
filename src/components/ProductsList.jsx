238a2b43e06ea280f72496e1a4ab6a45c84e2bed
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-18 16:32:37 +0100

    Product list partially completed

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -7,7 +7,7 @@ const ProductsList = () => {
   return (
     <div className='mt-12 grid gap-y-8'>
          {products.map((product)=>{
-          const {title,price,image} = product.attributes
+          const {title,price,image,company} = product.attributes
           const dollarsAmount = formatPrice(price);
           return (
               <Link 
@@ -21,8 +21,14 @@ const ProductsList = () => {
                    alt={title} 
                    className='h-24 w-24 rounded-lg sm:h-32 
                    sm:w-32 object-cover group-hover:scale-105 transition duration-300'
-                   
                    />
+                   <div className='ml-0 sm:ml-16'>
+                      <h3 className='capitalize font-medium text-lg'>{title}</h3>
+                      <h4 className='capitalize text-md text-neutral-content'>{company}</h4>
+                   </div>
+                   <p className='font-medium ml-0 sm:ml-auto text-lg'>
+                    {dollarsAmount}
+                   </p>
               </Link>
           );
           
