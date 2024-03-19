2bc4ebcf99f4c8ef6150cdbd886d18cc5ae46d2b
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-19 16:24:17 +0100

    Form Input modified

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,4 +1,4 @@
-const FormInput = ({ label, name, type, defaultValue }) => {
+const FormInput = ({ label, name, type, defaultValue, size }) => {
   return (
     <div className="form-control">
          <label className="label"> 
