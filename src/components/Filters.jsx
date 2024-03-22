a7c3698d2ad3cf7a6ef10a495260ab55f884e6c6
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-22 14:44:54 +0100

    Filter still in progress, with checkbox

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,6 +2,7 @@ import { Form, useLoaderData, Link } from 'react-router-dom';
 import FormInput from './FormInput';
 import FormSelect from './FormSelect';
 import FormRange from './FormRange';
+import FormCheckbox from './FormCheckbox';
 
 const Filters = () => {
   const {meta} = useLoaderData()
@@ -37,13 +38,15 @@ const Filters = () => {
       />
       {/* PRICE */}
       <FormRange name='price' label='select price' size='range-sm'/>
-   {/* BUTTONS */}
-   <button type='submit' className='btn btn-primary btn-sm'>
-    search
-   </button>
-   <Link to='/products' className='btn btn-accent btn-sm'>
-    reset
-   </Link>
+      {/* SHIPPING */}
+      <FormCheckbox name='shipping' label='free shipping' size='checkbox-sm'/>
+      {/* BUTTONS */}
+      <button type='submit' className='btn btn-primary btn-sm'>
+        search
+      </button>
+      <Link to='/products' className='btn btn-accent btn-sm'>
+      reset
+      </Link>
   </Form>
    
 }
