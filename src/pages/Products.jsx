94730b6d46ff28d284f3dae8e381d77fcf6a3169
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-24 18:37:05 +0100

    More update on filter changing ti to array of objects

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,9 +4,9 @@ import { customFetch } from "../utils";
 const url = '/products';
 
 export const loader = async ({ request }) =>{
-  const params = Object.fromEntries({
+  const params = Object.fromEntries([
       ...new URL(request.url).searchParams.entries(),
-  });
+  ]);
  console.log(params);
  const response = await customFetch(url);
  const products = response.data.data;
