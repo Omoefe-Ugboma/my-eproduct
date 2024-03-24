bce19a194ad99077112488f733296e3dd3dd6a77
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-24 18:27:22 +0100

    Filter still in progress inside the product file

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,6 +4,8 @@ import { customFetch } from "../utils";
 const url = '/products';
 
 export const loader = async ({ request }) =>{
+  const params = new URL(request.url).searchParams
+  const search = params.get('search')
   const response = await customFetch(url);
  const products = response.data.data;
  const meta = response.data.meta;
