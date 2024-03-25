8e4a7106d436b10aa0c57372c8d3d1debe007d83
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-25 18:55:43 +0100

    Adding the number of page

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,12 +1,20 @@
 import { useLoaderData, useLocation, useNavigate } from 'react-router-dom';
 const PaginationContainer = () => {
-  const {meta} = useLoaderData()
-  const {pageCount, page} = meta.pagination
-   return (
-    <div>
-      <h5>pagination</h5>
-    </div>
-  )
+  const {meta} = useLoaderData();
+  const {pageCount, page} = meta.pagination;
+
+    const pages = Array.from({ length: pageCount }, (_,index)=>{
+    return index + 1;
+    });
+    const handlePageChange = (pageNumber) =>{
+      console.log(pageNumber);
+    }
+    if(pageCount < 2) return null
+    return (
+      <div>
+        <h5>pagination</h5>
+      </div>
+    )
 }
 
 export default PaginationContainer;
