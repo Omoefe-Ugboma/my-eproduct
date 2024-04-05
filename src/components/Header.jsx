f82b63bae7a9ac9ce81d90d776ba6c60021aea25
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-05 20:42:55 +0100

    Logout access and user implement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
 import {  Link, useNavigate } from 'react-router-dom';
+import { clearCart } from '../features/cart/cartSlice';
+import { logoutUser } from '../features/user/userSlice';
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((state) => state.userState.user);
-  
+   
+  const handleLogout = () =>{
+    navigate('/');
+    dispatch(clearCart());
+    dispatch(logoutUser());
+  }
+
+
   return (
     <header className=' bg-neutral py-2 text-neutral-content '>
       <div className='align-element flex justify-center sm:justify-end '>
@@ -13,7 +22,10 @@ const Header = () => {
         {user ? (
           <div className='flex gap-x-2 sm:gap-x-8 items-center'>
           <p className='text-xs sm:text-sm'>Hello, {user.username}</p>
-          <button className='btn btn-xs btn-outline btn-primary'>
+          <button 
+            className='btn btn-xs btn-outline btn-primary'
+            onClick={handleLogout}
+            >
            logout
           </button>
         </div>
