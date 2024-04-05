24c9aa6601ef3cbb12bbfc6ce530945fead85605
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-05 19:55:45 +0100

    Logout and user access still in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userState.user);
+  
   return (
     <header className=' bg-neutral py-2 text-neutral-content '>
       <div className='align-element flex justify-center sm:justify-end '>
+        
+        {user ? <div className='flex gap-x-2 sm:gap-x-8 items-center'>
+          <p className='text-xs sm:text-sm'>Hello, {user.username}</p>
+          <button className='btn btn-xs btn-outline btn-primary'>
+           logout
+          </button>
+        </div>
+         }
         {/* USER */}
         {/* LINKS */}
         <div className='flex gap-x-6 justify-center items-center'>
