3efbb003945ddaddc9f73f4c851ee091fda5114d
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-06 21:20:06 +0100

    Register session in progress 1

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,10 @@
 import { FormInput, SubmitBtn} from '../components';
 import { Form, Link } from 'react-router-dom';
 
+export const action = async () =>{
+  return null;
+};
+
 const Register = () => {
   return <section className='h-screen grid place-items-center'>
       <Form method='POST' className='card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'>
