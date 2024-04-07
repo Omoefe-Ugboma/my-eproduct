aff6691d3fdab4b81ac4bd4db88269dca3e39a94
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-07 12:46:14 +0100

    User login setup in progress

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,10 @@
 import { FormInput, SubmitBtn } from "../components";
 import { Form, Link } from 'react-router-dom';
 
+export const action = async () =>{
+  
+} 
+
 const Login = () => {
   return <section className="h-screen grid place-items-center">
        <Form method="post" className="card w-96 p-8 bg-base-100 shadow-lg flex 
