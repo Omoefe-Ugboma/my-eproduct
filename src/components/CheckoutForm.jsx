6135b827989c3fe00c3cc999e075e5ed5c84d18c
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-12 16:03:27 +0100

    Checkout form in progress

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,3 +1,9 @@
+import { Form, redirect } from 'react-router-dom';
+import FormInput from './FormInput';
+import SubmitBtn from './SubmitBtn';
+import { customFetch, formatPrice } from '../utils';
+import { toast } from 'react-toastify';
+import { clearCart } from '../features/cart/cartSlice';
  
 const CheckoutForm = () => {
   return (
