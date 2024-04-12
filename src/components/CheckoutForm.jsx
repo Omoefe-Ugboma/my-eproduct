851902efbf568a48cfed51f5c6d5e6d79335aba1
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-12 16:17:15 +0100

    Check out form still  in progress 2

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -13,6 +13,11 @@ export const action = (store) => async () =>{
 const CheckoutForm = () => {
   return <Form method='POST' className='flex flex-col gap-y-4'>
    <h4 className='font-medium text-xl capitalize'>shipping information</h4>
+   <FormInput label='first name' name='name' type='text'/>
+   <FormInput label='address' name='address' type='text'/>
+    <div className='mt-4'>
+       <SubmitBtn text='place your order'/>
+    </div>
   </Form>
 }
 
