7802b04f97276df007a5455bad133da5cc48b68b
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-16 21:59:52 +0100

    Still in progress for order list

--- src/components/OrdersList.jsx
+++ src/components/OrdersList.jsx
@@ -4,12 +4,18 @@ import advancedFormat from 'dayjs/plugin/advancedFormat';
 day.extend(advancedFormat);
 
 const OrdersList = () => {
-  const {orders,meta} useLoaderData()
+  const {orders,meta} = useLoaderData()
   return  <div className='mt-8'>
      <h4 className='mb-4 capitalize'>total orders : {meta.pagination.total}</h4>
     <div className='overflow-x-auto'>
        <table className='table table-zebra'>
-
+         {/* head */}
+         <thead>
+          <tr>
+            <th>Name</th>
+            <th></th>
+          </tr>
+         </thead>
        </table>
     </div>
   </div>
