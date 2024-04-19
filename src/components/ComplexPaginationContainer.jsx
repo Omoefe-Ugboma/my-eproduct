7869bbb16dfb4a9dd65bc4ddd0b8026bb0bc658c
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-19 20:02:13 +0100

    Complex pagination completed

--- src/components/ComplexPaginationContainer.jsx
+++ src/components/ComplexPaginationContainer.jsx
@@ -30,6 +30,31 @@ const ComplexPaginationContainer = () => {
       const pageButtons = [];
       // first button
       pageButtons.push(addPageButton({pageNumber:1,activeClass: page === 1}));
+      
+      // dots
+     if(page > 2){
+        pageButtons.push(
+          <button className='join-item btn btn-xs sm:btn-md' key='dots-1'>
+            ...
+          </button>
+      );
+     }
+
+      // active/current page
+      if(page !== 1 && page !== pageCount){
+        pageButtons.push(addPageButton({pageNumber:page,activeClass: true}));
+      }
+
+        // dots
+        if(page < pageCount - 1){
+          pageButtons.push(
+            <button className='join-item btn btn-xs sm:btn-md' key='dots-2'>
+               ...
+            </button>
+         );
+        }
+       
+
       // last button
       pageButtons.push(
         addPageButton({pageNumber:pageCount,activeClass: page === pageCount})
