d75b952b974e1a5f89383e65b65ed48feca63d99
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-20 18:32:55 +0100

    React Query Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { 
   About,
   Cart,
