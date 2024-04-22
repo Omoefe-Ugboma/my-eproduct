7d6758f00329fa46902c7f1f1b2601fb09489795
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-04-22 20:52:13 +0100

    React Query - All products page completed

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,9 +2,27 @@ import { Filters, PaginationContainer, ProductsContainer } from "../components";
 import { customFetch } from "../utils";
 const url = '/products';
 
-const allProductsQuery = {queryParams} =>{
-  const {search, category, company, sort, price, shipping, page} = queryParams
-}
+const allProductsQuery = (queryParams) => {
+  const {search, category, company, sort, price, shipping, page} = 
+  queryParams;
+
+  return{
+    queryKey:[
+      'products',
+      search ?? '',
+      category ?? 'all',
+      company ?? 'all',
+      sort ?? 'a-z',
+      price ?? 100000,
+      shipping ?? false,
+      page ?? 1,
+    ],
+    queryFn: () => 
+     customFetch(url,{
+        params:queryParams,
+      }),
+  };
+};
 
 export const loader = 
 (queryClient) => 
@@ -13,9 +31,7 @@ export const loader =
       ...new URL(request.url).searchParams.entries(),
   ]);
 //  console.log(params);
- const response = await customFetch(url,{
-  params
- });
+ const response = await queryClient.ensureQueryData(allProductsQuery(params))
  const products = response.data.data;
  const meta = response.data.meta;
  return { products, meta, params };
